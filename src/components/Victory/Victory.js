96239b790faa455f8241b36360dacf1f8df919ad
Julio Cesar Castro Macias <83622621+lmEkoz@users.noreply.github.com>
2022-06-18 18:28:07 -0500

    commit Vicroty changes

--- src/components/Victory/Victory.js
+++ src/components/Victory/Victory.js
@@ -4,6 +4,7 @@ import { VictoryChart, VictoryLine } from "victory";
 import Sliders from "../Sliders/Sliders";
 
 const Victory = (props) => {
+    console.log(props);
     return (
         <div id="charts">
             <VictoryChart height={300} width={300}>
@@ -14,7 +15,7 @@ const Victory = (props) => {
                     }}
                     data={[
                         { x: 0, y: 0 },
-                        { x: 20, y: 20 },
+                        { x:20, y: 20 },
                     ]}
                     />
                 </VictoryChart>
